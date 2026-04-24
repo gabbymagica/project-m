bff8541005c22ff72fa7e1db490bf321fac9b23d
gabbymagica <gabrielcecote1@gmail.com>
2026-04-24 17:15:47 -0300

    tile debug

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type Entity struct {
@@ -15,7 +16,12 @@ type Entity struct {
 	Sprite *ebiten.Image
 }
 
+type Object struct {
+	Sprite *ebiten.Image
+}
+
 type Game struct {
+	Map      [36][240]*Object
 	Player   *Entity
 	Entities []*Entity
 	Inputs   []ebiten.Key
@@ -25,24 +31,35 @@ func (g *Game) Update() error {
 	g.Inputs = inpututil.AppendPressedKeys(g.Inputs[:0])
 
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		g.Player.X += 2.5
+		g.Player.X += 5
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		g.Player.X -= 2.5
+		g.Player.X -= 5
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		g.Player.Y += 2.5
+		g.Player.Y += 5
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		g.Player.Y -= 2.5
+		g.Player.Y -= 5
 	}
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+
+	
+	for y := range g.Map {
+		for x := range g.Map[y] {
+			x1 := float32(x * 16)
+			y1 := float32(y * 16)
+			vector.StrokeLine(screen, x1, y1, x1+16, y1, 1, color.RGBA{0xFF, 0xFF, 0xFF, 0x00}, false)
+			vector.StrokeLine(screen, float32(x1+16), y1, float32(x1+16), float32(y1+16), 1, color.RGBA{0xFF, 0xFF, 0xFF, 0x00}, false)
+		}
+	}
+
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(g.Player.X, g.Player.Y)
+	op.GeoM.Translate(960/2, 540/2)
 	if g.Player.Sprite != nil {
 		screen.DrawImage(g.Player.Sprite, op)
 	}
@@ -60,7 +77,7 @@ func main() {
 
 	game := &Game{}
 
-	el_quadrado_vermelho := ebiten.NewImage(32, 32)
+	el_quadrado_vermelho := ebiten.NewImage(16, 16)
 	el_quadrado_vermelho.Fill(color.RGBA{0xff, 0, 0, 0xff})
 
 	game.Player = &Entity{
